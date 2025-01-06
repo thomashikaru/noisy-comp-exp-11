5c889b2d818432a331c481c4be43499210431907
anonymous <thclark@mit.edu>
2025-01-06 10:27:13 -0500

    first commit

---
+++ src/magpie.config.js
@@ -0,0 +1,13 @@
+export default {
+  experimentId: "11",
+  serverUrl: "https://noisy-comp-server-311aa565092d.herokuapp.com",
+  socketUrl: "wss://noisy-comp-server-311aa565092d.herokuapp.com/socket",
+  // this will be used in prolific mode
+  completionUrl: "https://app.prolific.com/submissions/complete?cc=C5QR6PZ1",
+  contactEmail: "thclark@mit.edu",
+  // Either 'debug', 'directLink' or 'prolific'
+  // mode: "prolific",
+  // mode: "debug",
+  mode: "directLink",
+  language: "en",
+};
